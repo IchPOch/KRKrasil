607db9eb2dcc76f91ca303b0a23c3e1a922f8f88
Михаил <@DESKTOP-3P2543V>
2022-04-22 14:31:11 +0300

    Добавьте файлы проекта.

---
+++ KRKrasil/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+namespace KRKrasil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"USR.txt";
+            using (StreamWriter file1 = new StreamWriter(path))
+            {
+                
+                RegistryKey rk = Registry.Users;
+
+                string[] names = rk.GetSubKeyNames();
+
+
+                textBox1.Text += rk.Name;
+                textBox1.Text += Environment.NewLine;
+
+                foreach (string s in names)
+                {
+                    file1.WriteLine(s);
+                    textBox2.Text += s;
+                    textBox2.Text += Environment.NewLine;
+                }
+            }
+
+        }
+    }
+}
